6812e7c552ecbf1074d7fcf43f3b4e94441cafbc
BENO1998 <108861498+BENO1998@users.noreply.github.com>
2022-10-05 20:56:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Login from "./Components/login";
+import Button from "./Components/Button";
+import Dashboard from "./Components/Dashboard";
+import Cards from './Components/Cards';
+import Tables from './Components/Table';
+import Color from './Components/color';
+import Borders from './Components/border';
+import Animation from './Components/animation';
+import OtherComponent from './Components/other';
+import Register from './Components/Register';
+import ForgotPassword from './Components/ForgotPassword';
+import Component404 from './Components/component404';
+import BlankPage from './Components/blankpage';
+import Charts from './Components/Charts';
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Dashboard/>}/>
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Button" element={<Button/>}/>
+      <Route path="/Cards" element={<Cards/>}/>
+      <Route path="/Table" element={<Tables/>}/>
+      <Route path="/color" element={<Color/>}/>
+      <Route path="/borders" element={<Borders/>}/>
+      <Route path="/animation" element={<Animation/>}/>
+      <Route path="/other" element={<OtherComponent/>}/>
+      <Route path="/Register" element={<Register/>}/>
+      <Route path="/ForgotPassword" element={<ForgotPassword/>}/>
+      <Route path="/component404" element={<Component404/>}/>
+      <Route path="/blankpage" element={<BlankPage/>}/>
+      <Route path="/Charts" element={<Charts/>}/>
+     </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
